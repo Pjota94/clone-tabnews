04e8dd428de89fb4ba6ce6dd42acfa0688de951e
Paulo Junior <98767739+Pjota94@users.noreply.github.com>
2024-10-30 20:05:41 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
-function Home(){
-    return <h1>“Eu sou o Caminho, a Verdade e a Vida. Ninguém vai a Pai senão por mim!” (Jo 14,6)</h1>
+function Home() {
+  return (
+    <h1>
+      “Eu sou o Caminho, a Verdade e a Vida. Ninguém vai a Pai senão por mim!”
+      (Jo 14,6)
+    </h1>
+  );
 }
 
 export default Home;
