8e12c31f2c09dd7e37a87162e32ea8248c7b5e68
paulojunior <juniorgurinhem9@gmail.com>
2025-03-08 11:38:33 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 function Home() {
-    return <h1>Jesus é o caminho a verdade e a vida! Ninguém vai ao pai se não por Ele...</h1>
+  return (
+    <h1>
+      Jesus é o caminho a verdade e a vida! Ninguém vai ao pai se não por Ele...
+    </h1>
+  );
 }
 
 export default Home;
